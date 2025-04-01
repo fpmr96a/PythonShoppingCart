d0718decd4b15d259ccbe2cf9b4faea2cfeb5a9c
Sam Faraclas <samuel.faraclas@rt2.com>
2025-04-01 18:53:14 -0400

    small changes

--- shopping/main.py
+++ shopping/main.py
@@ -1,4 +1,6 @@
-import utility
-import shopping.shopping_cart
+from utility import multiply, divide
+from shopping.more_shopping import shopping_cart
 
-print(shopping.shopping_cart.buy('apple'))
+print(shopping_cart.buy('apple'))
+print(divide(5,2))
+print(multiply(5,2))
